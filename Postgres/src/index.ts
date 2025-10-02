b100ca71110e9500b38fed731534817ebb18d62e
Yogesh <yogeshpatil0811d@gmail.com>
2025-10-02 17:37:11 +0530

    Postgres class done

--- Postgres/src/index.ts
+++ Postgres/src/index.ts
@@ -28,10 +28,13 @@ async function main(){
 main();
 
 app.post("/signup",async (req,res)=>{
-    const {username,email,password} = req.body;
+    const {username,email,password,city,country,street,pincode} = req.body;
     try{
-        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`
         const response =  await pgClient.query(insertQuery, [username,email,password]);
+        const userId = response.rows[0].id;
+        const addressInsertQuery = `INSERT INTO addresses (city,country,street,pincode,user_id) VALUES ($1,$2,$3,$4,$5);`
+        const addressInsertResponse = await pgClient.query(addressInsertQuery, [city,country,street,pincode, userId])
         res.json({
             message: "You have signed up"
         })
@@ -41,6 +44,28 @@ app.post("/signup",async (req,res)=>{
             message: "Error signing up"
         })
     }
+})
+
+app.get("/metadata",async (req,res)=>{
+    const id = req.query.id;
+    const query1 = 'SELECT username, email, id FROM users WHERE id=$1';
+    const response1 = await pgClient.query(query1, [id]);
+    const query2 = 'SELECT * FROM addresses WHERE user_id=$1';
+    const response2 = await pgClient.query(query2, [id]);
+
+    res.json({
+        user: response1.rows[0],
+        address: response2.rows[0]
+    })
+})
+
+app.get("/better-metadata", async (req,res)=>{
+    const id = req.query.id;
+    const query = 'SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street, addresses.pincode FROM users JOIN addresses ON users.id = addresses.user_id WHERE users.id = $1;'
+    const response = await pgClient.query(query, [id]);
+    res.json({
+        response: response.rows
+    })
 })
 
 app.listen(3000,()=>{
