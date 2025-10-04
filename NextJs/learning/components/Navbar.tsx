9400b75fd49fa855e8900562a191dddfd7f04aad
Yogesh <yogeshpatil0811d@gmail.com>
2025-10-04 22:26:18 +0530

    Learning NextJs

---
+++ NextJs/learning/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar(){
+    return (
+        <>
+            <div className="border-b p-4">
+                Life is Good
+            </div>
+            <br />
+        </>
+    )    
+}
