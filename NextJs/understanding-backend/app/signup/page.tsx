7ac4d1bddb6bde445505f9307c9881f12037696e
Yogesh <yogeshpatil0811d@gmail.com>
2025-10-17 09:05:45 +0530

    19th week completed

--- NextJs/understanding-backend/app/signup/page.tsx
+++ NextJs/understanding-backend/app/signup/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import {useState} from "react";
 
 export default function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const router = useRouter();
 
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -23,11 +25,12 @@ export default function Signup() {
                         <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={e=>{
                             setPassword(e.target.value);
                         }}/>
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={()=>{
-                            axios.post("http://localhost:3000/api/v1/signup",{
+                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={async ()=>{
+                            await axios.post("http://localhost:3000/api/v1/signup",{
                                 username,
                                 password
                             })
+                            router.push("/signin");
                         }}>Sign up</button>
                     </div>
                 </div>
